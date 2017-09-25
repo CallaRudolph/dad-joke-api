db30f0665c7de144f23fba4af7cd7db830afa32a
Calla Rudolph <calla@>
2017-09-25 15:08:14 -0700

    write handleSubmit method to retrieve user input

--- src/components/JokeInput.jsx
+++ src/components/JokeInput.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { connect } from "react-redux";
 
-function JokeInput(props){
-  return (
-    <div>
-      <input placeholder="Type in a word."></input>
-      <button>I am a button.</button>
-    </div>
-  );
+class JokeInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let { _word } = this.refs;
+    if (!_word.value.trim()) {
+      return;
+    }
+    this.props.dispatch(fetchJoke(_word.value.trim()));
+    _word.value = "";
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input placeholder="Type in a word." ref="_word"></input>
+          <button>I am a button.</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default JokeInput;
+export default connect()(JokeInput);
