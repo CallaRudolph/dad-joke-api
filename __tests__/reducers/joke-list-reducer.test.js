192d718096d082b79f6604c5464f773def850e51
Calla Rudolph <callarudolph@gmail.com>
2017-09-26 15:10:05 -0700

    pass third test

--- __tests__/reducers/joke-list-reducer.test.js
+++ __tests__/reducers/joke-list-reducer.test.js
@@ -5,35 +5,30 @@ import * as actions from "./../../src/actions";
 describe("Joke list reducer", () => {
   const { defaultState, types } = constants;
 
-  test('should return equivalent state if no action type is recognized', () => {
+  it('should return equivalent state if no action type is recognized', () => {
     expect(jokesById([], { type: null })).toEqual([]);
   });
 
   it("update state on request joke", () => {
-      const action = actions.requestJoke("cheese");
-      const newObject = {
-        isFetching: true,
-        word: action.word,
-        jokeId: action.jokeId,
-      };
-      expect(jokesById(defaultState.jokeId, action)[action.jokeId])
-      .toEqual(newObject);
-    });
+    const action = actions.requestJoke("cheese");
+    const newObject = {
+      isFetching: true,
+      word: action.word,
+      jokeId: action.jokeId,
+    };
+    expect(jokesById(defaultState.jokesById, action)[action.jokeId])
+    .toEqual(newObject);
+  });
 
-    it("update state on receive joke", () => {
-      const action = actions.receiveSong("kiss", "prince", 1, ["you don't have to be beautiful", "to turn me on"]);
-      const newObject = {
-        isFetching: false,
-        title: action.title,
-        artist: action.artist,
-        songId: action.songId,
-        receivedAt: action.receivedAt,
-        songArray: action.songArray,
-        currentPhrase: action.songArray[0],
-        arrayPosition: 0
-      };
-      expect(songsById(defaultState.songsById, action)[action.songId])
-      .toEqual(newObject);
-    });
+  it("update state on receive joke", () => {
+    const action = actions.displayJoke("This is a joke", 1);
+    const newObject = {
+      isFetching: false,
+      joke: action.joke,
+      jokeId: action.jokeId
+    };
+    expect(jokesById(defaultState, action))
+    .toEqual(newObject);
+  });
 
 });
